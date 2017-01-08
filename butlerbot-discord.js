015e71d557f7da2cdb726f156282991209abe74b
Tiffany Le-Nguyen <letiffany.nguyen@gmail.com>
2017-01-08 16:42:34 -0500

    Add discord implementation

---
+++ butlerbot-discord.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const butlerbot = new Discord.Client();
+const token = 'MjY3NzU0NzgyMzgxNDQxMDM2.C1Q8UQ.MhW8oPNdEOne_-mn4ONnGb5rpYQ';
+
+// turn butlerbot on, make sure it's ready before receiving messages
+butlerbot.on('ready', () => {
+	console.log('butlerbot is ready to serve...');
+});
+
+// event listener for messages
+butlerbot.on('message', message => {
+	const msg = message.content;
+	if (msg === 'butler' || msg === 'butlerbot') {
+		message.reply('greetings master.');
+	}
+
+	if (msg === 'who\'s the idiot' || msg === 'idiot') {
+		message.reply(message.author.avatarURL);
+	}
+});
+
+butlerbot.login(token);
